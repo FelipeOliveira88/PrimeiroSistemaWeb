862de56b3320283de61e1eaba12264a2111ae26b
Felipe Oliveira <fmsoliveira@live.com>
2020-10-08 16:41:52 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "SallesWeb MVC App from C# Course";
+            ViewData["professor"] = "Nelio Alvez";
 
             return View();
         }
